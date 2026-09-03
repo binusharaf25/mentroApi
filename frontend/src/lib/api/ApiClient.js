7d333de77810f9131f638bf786b0436c25ae73a3
Your Name <you@example.com>
2026-09-03 09:04:49 +0100

    removed dist

--- frontend/src/lib/api/ApiClient.js
+++ frontend/src/lib/api/ApiClient.js
@@ -1,30 +1,23 @@
-import axios from "axios"
-import { config } from "dotenv"
-import useAuthStore from "../stores/authStore"
+import axios from "axios";
+import useAuthStore from "../stores/authStore";
 
+const api_url = "https://mentroapi.onrender.com/api";
 
+const Api = axios.create({
+  baseURL: api_url,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
+Api.interceptors.request.use((config) => {
+  const token = useAuthStore.getState().token;
 
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
+  return config;
+});
 
-const api_url='https://mentroapi.onrender.com/api'
-
-const Api=axios.create({
-    baseURL:api_url,
-    headers:{
-        'Content-type':'application/json'
-    }
-})
-
-//interceptor to add  the authorization header
-
-Api.interceptors.request.use((config)=>{
-    const token= useAuthStore.getState().token;
-
-    if(token){
-        config.headers.Authorization=`bearer ${token}`
-    }
-    return config;
-})
-
-export default Api
+export default Api;
